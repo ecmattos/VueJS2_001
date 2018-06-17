e349c9510e5bde0d8a1fae0068417b6b85ba06ef
eduardo <ecmattos@ig.com.br>
2018-06-17 02:34:35 -0300

    Mais projetos

---
+++ myProject/src/components/TaskComponent.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="comp">
+		<h1>{{ title }}</h1>
+		<table>
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Completado</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(task, index) in tasks" :key="index">
+					<td>{{ task.name }}</td>
+					<td>{{ task.completed ? 'Sim' : 'Não' }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '#Task Component',
+				tasks: [
+					{name: 'Lavar louca', completed: true},
+					{name: 'Cortar grama', completed: true},
+					{name: 'Lavar carro', completed: true},
+					{name: 'Lavar roupas', completed: true},
+					{name: 'Lavar piso', completed: true}
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.comp {
+		border: 1px solid red;
+		margin: 5px;
+		padding: 5px;
+	}
+</style>
---
+++ myProject/src/components/TestComponent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="comp">
+		<h1>{{ title }}</h1>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				title: '#Test Component'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	p{
+		color: red
+	}
+	.comp {
+		border: 1px solid green;
+		margin: 5px;
+		padding: 5px;
+	}
+</style>
+
